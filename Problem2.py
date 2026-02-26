fd4e296b6c3f8ebce34ac9e1b8925907d7160218
Home <home@Homes-MacBook-Pro.local>
2026-02-26 18:00:11 -0500

    added preorder traversal code here

--- Problem2.py
+++ Problem2.py
@@ -1,4 +1,16 @@
 from Problem1 import exp_tree
 # using prev problems code 
 
+# Using preorder traversal here
+# ie root -> left -> right
+def preorder_trav(root):
+
+    if root is None:
+        return []
+
+    result = [root.value]
+    result += preorder_trav(root.left)
+    result += preorder_trav(root.right)
+
+    return result
 
