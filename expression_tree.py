06f67e65dc66e01e99d477600621b052f8bbe5e5
Home <home@Homes-MacBook-Pro.local>
2026-02-26 17:03:07 -0500

    added testcase to see if my program works

--- expression_tree.py
+++ expression_tree.py
@@ -4,22 +4,26 @@ class Node:
         self.left = None
         self.right = None
 
-    def exp_tree():
-        stack = []
-        operators = {"+", "-", "*", "/"}
+def exp_tree(postfix_list):
+    stack = []
+    operators = {"+", "-", "*", "/"}
 
-        for token in postfix_list:
-            if token not in operators:
-                node = Node(token)
-                stack.append(node)
-            else:
-                right = stack.pop()
-                left = stack.pop()
+    for token in postfix_list:
+        if token not in operators:
+            node = Node(token)
+            stack.append(node)
+        else:
+            right = stack.pop()
+            left = stack.pop()
 
-                node = Node(token)
-                node.left = left
-                node.right = right
+            node = Node(token)
+            node.left = left
+            node.right = right
+            stack.append(node)
 
-                stack.append(node)
-
-        return stack[0]
+    return stack[0]
+    
+if __name__ == "__main__":
+    postfix = ["3", "4", "+", "2", "*"]
+    root = exp_tree(postfix)
+    print(root.value)  
