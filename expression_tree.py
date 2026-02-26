ec9cef9c19a1fd6de4d43865490bb7cd11436263
Home <home@Homes-MacBook-Pro.local>
2026-02-26 17:07:46 -0500

    added a few comments for readability

--- expression_tree.py
+++ expression_tree.py
@@ -1,9 +1,14 @@
+# Problem 1
+
+# adding a basic node class here
 class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
+
+# converting the postfix into a expression tree here
 def exp_tree(postfix_list):
     stack = []
     operators = {"+", "-", "*", "/"}
@@ -19,11 +24,14 @@ def exp_tree(postfix_list):
             node = Node(token)
             node.left = left
             node.right = right
+
             stack.append(node)
 
     return stack[0]
-    
+
+
+# small test case to see if my programme works
 if __name__ == "__main__":
     postfix = ["3", "4", "+", "2", "*"]
     root = exp_tree(postfix)
-    print(root.value)  
+    print(root.value)
