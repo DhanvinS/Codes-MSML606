dfd7e01b597c6af8d45588606278c5655c8ef558
Home <home@Homes-MacBook-Pro.local>
2026-02-26 18:39:27 -0500

    Implemented custom Stack ADT with manual top management

--- Problem3.py
+++ Problem3.py
@@ -8,17 +8,17 @@ class Stack:
         return self.top == -1
 
     def push(self, value):
-        # Pushing element onto stack.
+        # Pushing element  onto stack.
         self.data.append(value)
         self.top += 1
 
     def pop(self):
         
        # Removing and returning top element.
-       # Raises error if stack is empty so will use an exceptio nstatement.
+       # Raises error if stack is empty so  will use an exceptio nstatement.
         
         if self.is_empty():
-            raise IndexError("Popping from an empty stack")
+            raise IndexError("Popping from an  empty stack")
 
         value = self.data[self.top]
         self.data.pop()
@@ -28,6 +28,56 @@ class Stack:
     def peek(self):
         # Returning top element not removing it tho
         if self.is_empty():
-            raise IndexError("Peek from empty stack")
+            raise IndexError("Peek  from empty stack")
 
         return self.data[self.top]
+    
+def evaluate_postfix(expression):
+    """
+    Evaluating a postfix expression using a stack here
+
+    Parameters im  using :
+        expression : space-separated postfix expression
+
+    This returns a  float or int: result of evaluation
+    """
+
+    if not expression:
+        return None
+
+    stack = Stack()
+    operators = {"+", "-", "*", "/"}
+
+    tokens = expression.split()
+
+    for token in tokens:
+
+        # If operand → push as number
+        if token not in operators:
+            stack.push(float(token))
+
+        # If operator → pop two operands and apply operation
+        else:
+            if stack.top < 1:
+                raise ValueError("Invalid postfix expression")
+
+            right = stack.pop()
+            left = stack.pop()
+
+            if token == "+":
+                result = left + right
+            elif token == "-":
+                result = left - right
+            elif token == "*":
+                result = left * right
+            elif token == "/":
+                if right == 0:
+                    raise ZeroDivisionError("Division by zero")
+                result = left / right
+
+            stack.push(result)
+
+    if stack.top != 0:
+        raise ValueError("Invalid postfix expression")
+
+    return stack.pop()
