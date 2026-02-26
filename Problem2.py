bb419c4619b026d9b8dfa3b29fba70ff384e795d
Home <home@Homes-MacBook-Pro.local>
2026-02-26 18:07:50 -0500

    added postorder traversal code here

--- Problem2.py
+++ Problem2.py
@@ -4,7 +4,6 @@ from Problem1 import exp_tree
 # Using preorder traversal here
 # ie root -> left -> right
 def preorder_trav(root):
-
     if root is None:
         return []
 
@@ -14,3 +13,15 @@ def preorder_trav(root):
 
     return result
 
+# using postorder traversla here 
+# left -> right -> root
+def postorder_trav(root):
+    if root is None:
+        return []
+
+    result = []
+    result += postorder_trav(root.left)
+    result += postorder_trav(root.right)
+    result.append(root.value)
+
+    return result
